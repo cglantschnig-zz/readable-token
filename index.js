292fb3174a96f395958dd0bd1e36b3e49ffd4c61
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-03-05 23:00:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var Generator = function() {
+
+  var words = require('./en/nouns.json').words;
+
+  var random = getRandomInt(0, words.length - 1);
+
+  return words[random] + getRandomInt(1, 100);
+
+};
+
+
+module.exports = Generator;
