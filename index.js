5c4a6f4df3f319af8e752b27c42c1da39d9a52af
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-05-31 16:45:35 +0200

    added function comments

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-
+/**
+ * returns a random integer between min and max
+ * @returns int
+ */
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var Generator = function() {
+/**
+ * returns a random word from the choosen list
+ */
+var Generator = function(language, list) {
+  if (!language) language = 'en';
+  if (!list) list = 'nouns';
 
-  var words = require('./words/en/nouns.json').words;
+  var words = [];
+  try {
+    words = require('./words/' + language + '/' + list + '.json').words;
+  }
+  catch(e) {
+    throw new Error('Could not find a word list with the given parameters!');
+  }
 
   var random = getRandomInt(0, words.length - 1);
 
